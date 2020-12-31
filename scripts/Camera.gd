c9355a37e6cf1196fd772c7a1c93d97a8f56cc9c
Paweł S <theoptimizersgroup@gmail.com>
2020-12-31 13:27:54 +0100

    Player updated

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,8 +1,8 @@
 extends Camera
 
 
-export(float, 0, 2) var rotation_speed := 1.0
+export(NodePath) onready var target_body = get_node(target_body) as KinematicBody
+export(float, 0, 2) var mouse_sensitivity := 1.0
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	pass
--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -1,8 +1,30 @@
 extends KinematicBody
 
 
+export var _move_speed := 30.0
+export(float, 0, 2) var mouse_sensitivity := 1.0
+
+onready var camera := $Camera
+
+var direction_vector := Vector3.ZERO
+Area
+
+func _physics_process(delta: float) -> void:
+	var forward_movement := Vector3.ZERO
+	var sideways_movement := Vector3.ZERO
+	move_and_slide(direction_vector * _move_speed)
+
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("move_forward"):
-		pass
-	elif event.is_action("move_backward"):
-		pass
+	direction_vector = Vector3(
+			event.get_action_strength("strafe_right") - event.get_action_strength("strafe_left"),
+			0,
+			event.get_action_strength("move_backward") - event.get_action_strength("move_forward")
+		).normalized()
+	
+	var mouse_motion_event := event as InputEventMouseMotion
+	if mouse_motion_event:
+		rotation_degrees.y -= mouse_motion_event.relative.x * mouse_sensitivity
+		var camera_tilt: float = camera.rotation_degrees.x
+		camera_tilt -= mouse_motion_event.relative.y * mouse_sensitivity
+		camera.rotation_degrees.x = clamp(camera_tilt, -90, 90)
