bdeaa220aea0f19559ebe0aec4099ea7f019755c
Paweł S <theoptimizersgroup@gmail.com>
2021-01-03 15:03:13 +0100

    Removed unused files

--- scripts/Camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera
-
-
-export(NodePath) onready var target_body = get_node(target_body) as KinematicBody
-export(float, 0, 2) var mouse_sensitivity := 1.0
-
-
-func _unhandled_input(event: InputEvent) -> void:
