1cdcdc35ef141a4cb5fa5d426de4c76e6ad560ed
haennes <hannes.hofmuth@gmail.com>
2021-04-06 21:06:12 +0200

    overhaul of FuelTanks, added Tank class, fuel now get consumed

--- scripts/RocketParts/Engine/Engine.gd
+++ scripts/RocketParts/Engine/Engine.gd
@@ -6,23 +6,40 @@ var thrust_percentage := 0.9
 var on := false
 var connected_tank
 var fuel_configs = [] # for example: [   [  ["Hydrogen", 2],["Oxygen",1],Curve_consumption  ], next option    ]  save this in a resource later
-var current_config = [["Hydrogen",2],["Oxygen",1],Curve.new()] # one of the fuel configs from fuel configs
+var current_config = [["Hydrogen",2],["Oxygen",1]] # one of the fuel configs from fuel configs
+var consumption_curve
 
-func _integrate_engine_thrust(state) -> void:
+func _ready():
+	consumption_curve = Curve.new()
+	consumption_curve.add_point(Vector2(0,0))
+	consumption_curve.add_point(Vector2(100,100))
+
+func _integrate_engine_thrust(_state) -> void:
 	var force : Vector3 = thrust_total * thrust_percentage * global_transform.basis.y * float(on)
 	var offset : Vector3 = get_node("offset").translation
 	add_force(force,offset)
+	_consume_fuel()
+	print(on)
 
 func _consume_fuel():
-	var consumption = current_config.pop_back()
-	var fuels = current_config
-
-	for i in fuels:
-		connected_tank.consume_fuel(i[1]*curve_consuption.interpolate_baked(thrust_percentage),i[0]) # wont work
-
+	var return_values = []
+	if connected_tank == null:
+		return
+	for i in current_config:
+		return_values.append(connected_tank.consume_fuel_auto(i[1]*consumption_curve.interpolate_baked(thrust_percentage * float(on)),i[0]))
+	
+	var one_is_neg = false
+	for i in return_values:
+		if i < 0:
+			one_is_neg = true
+			break
+	if on and one_is_neg:
+		cutoff_engine()
+	
 
 func _integrate_forces(state):
 	_integrate_engine_thrust(state)
+	add_central_force(Vector3.DOWN)
 	
 func connect_tank(tank):
 	connected_tank = tank
---
+++ scripts/RocketParts/FuelTank/Tank.gd
@@ -0,0 +1,80 @@
+class_name Tank
+extends Resource
+
+export var possible_fuels = ["Hydrogen"]
+export var max_capacity = 50000
+export var transfer_rate = 20
+var current_fuel_name = ""
+var current_fuel_amount = 0
+
+
+func add_fuel(amount : int, fuel_name : String):
+	if can_currently_hold_fuel(fuel_name):
+			set_current_fuel(fuel_name)
+	else:
+		return "you cant store " + fuel_name + "in this tank"
+	if amount < 0:
+		return "amount smaller than 0, NOTE: you cant remove/consume fuel by that use consume(amount,fuel_name) instead"
+	if amount + current_fuel_amount < max_capacity:
+		_add_fuel_unsafe(amount)
+		return 0
+	else:
+		var fuel_left = -(max_capacity - current_fuel_amount - amount)
+		_add_fuel_unsafe(amount-fuel_left)
+		return fuel_left #retuns how much fuel couldnt be added
+
+func _add_fuel_unsafe(amount : int):
+	current_fuel_amount += amount
+	pass
+
+	
+func consume_fuel(amount : int):
+	print("consumed fuel current fuel"+ current_fuel_name+ "amount:"+ String(current_fuel_amount))
+	if amount < 0:
+		return "amount smaller than 0, NOTE: you cant add fuel by that use add_fuel(amount,fuel_name) instead"
+	if amount > current_fuel_amount:
+		_consume_fuel_unsafe(current_fuel_amount)
+		return current_fuel_amount - amount #returns how much fuel couldnt be withdrawn (negative)
+	else:
+		_consume_fuel_unsafe(amount)
+		return 0
+
+func _consume_fuel_unsafe(amount : int):
+	current_fuel_amount -= amount
+
+
+func can_hold_fuel(fuel_name : String):
+	return fuel_name in possible_fuels
+
+func can_currently_hold_fuel(fuel_name : String):
+	return can_hold_fuel(fuel_name) and (get_current_fuel() == "" or get_current_fuel() == fuel_name)
+
+func get_possible_fuels():
+	return possible_fuels
+
+func get_current_fuel():
+	return current_fuel_name
+
+func set_current_fuel(name: String):
+	if can_hold_fuel(name):
+		current_fuel_name = name
+	else:
+		return "This tank cant hold " + name
+
+func is_full(with = 0) -> bool:
+	return current_fuel_amount + with >= max_capacity
+
+static func get_most_filled_tank(tanks_array):
+	var current_max = tanks_array[0]
+	for i in tanks_array:
+		if current_max.current_fuel_amount < i.current_fuel_amount:
+			current_max = i
+	return current_max
+
+		
+static func get_biggest_tank(tanks_array):
+	var current_max = tanks_array[0]
+	for i in tanks_array:
+		if current_max.max_capacity < i.max_capacity:
+			current_max = i
+	return current_max
